8f3c679f1866d949696914af55bd4e4f4ba10368
JOSE VICTOR SANTIAGO FIGUEIREDO BAPTISTA <78240695+jvictorbap@users.noreply.github.com>
2021-09-21 16:28:05 -0300

    push components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { Login } from './components';
 
 function App() {
+  useEffect(() => {
+    const user = { email: '' };
+    localStorage.setItem('mealsToken', '1');
+    localStorage.setItem('cocktailsToken', '1');
+    localStorage.setItem('user', JSON.stringify(user));
+  }, []);
+
   return (
-    <div className="App">
-      <h1>Trybe</h1>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+      </Switch>
+    </BrowserRouter>
+    // <h1>Teste</h1>
   );
 }
 
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as Login } from './Login';
+export { default as User } from './User';
+// export {default as Comp3} from './Comp3.jsx';
