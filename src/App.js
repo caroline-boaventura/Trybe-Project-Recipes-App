d8fd0cb432f5fca0ee7dd2f4b4d96d0a8879bdfa
JOSE VICTOR SANTIAGO FIGUEIREDO BAPTISTA <78240695+jvictorbap@users.noreply.github.com>
2021-09-21 18:15:51 -0300

    pages e components organizados e rotas criadas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
 import React from 'react';
 import './App.css';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { Login, Foods } from './components';
+import {
+  Login,
+  Foods,
+  Drinks,
+  EspecificDrink,
+  EspecificFood,
+  EspecificFoodProgress,
+  ExploreAll,
+  ExploreFoods,
+  ExploreDrinks,
+  ExploreFoodsByIngredient,
+  ExploreDrinksByIngredient,
+  ExploreFoodByOrigin,
+  User,
+  DoneRecipes,
+  FavoriteRecipes } from './pages';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route path="/comidas/:id" component={ EspecificFood } />
+        <Route path="/bebidas/:id" component={ EspecificDrink } />
         <Route exact path="/comidas" component={ Foods } />
+        <Route exact path="/bebidas" component={ Drinks } />
+        <Route
+          exact
+          path="/comidas/:id/in-progress"
+          component={ EspecificFoodProgress }
+        />
+        <Route exact path="/bebidas/:id/in-progress" component={ Foods } />
+        <Route exact path="/explorar" component={ ExploreAll } />
+        <Route exact path="/explorar/comidas" component={ ExploreFoods } />
+        <Route exact path="/explorar/bebidas" component={ ExploreDrinks } />
+        <Route
+          exact
+          path="/explorar/comidas/ingredientes"
+          component={ ExploreFoodsByIngredient }
+        />
+        <Route
+          exact
+          path="/explorar/bebidas/ingredientes"
+          component={ ExploreDrinksByIngredient }
+        />
+        <Route exact path="/explorar/comidas/area" component={ ExploreFoodByOrigin } />
+        <Route exact path="/perfil" component={ User } />
+        <Route exact path="/receitas-feitas" component={ DoneRecipes } />
+        <Route exact path="/receitas-favoritas" component={ FavoriteRecipes } />
       </Switch>
     </BrowserRouter>
   );
---
+++ src/pages/ExploreAll.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function ExploreAll() {
+  return (
+    <h1>Explorar Drinks e Comidas</h1>
+  );
+}
