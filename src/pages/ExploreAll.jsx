df021be67085698a145b75f6a479a61ae5398e1d
JOSE VICTOR SANTIAGO FIGUEIREDO BAPTISTA <78240695+jvictorbap@users.noreply.github.com>
2021-09-29 16:47:47 -0300

    faltou ex 77

--- src/pages/ExploreAll.jsx
+++ src/pages/ExploreAll.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Header, Footer } from '../components/index';
 
 export default function ExploreAll() {
   return (
     <div>
       <Header title="Explorar" visibility={ false } />
+      <Link
+        to="/explorar/comidas"
+      >
+        <button data-testid="explore-food" type="button">Explorar Comidas</button>
+      </Link>
+      <Link
+        to="/explorar/bebidas"
+      >
+        <button data-testid="explore-drinks" type="button">Explorar Bebidas</button>
+      </Link>
       <Footer />
     </div>
   );
